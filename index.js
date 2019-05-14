e0cc2a6aa9324c775ce9601bb2519efc0a407c2d
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-14 17:05:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,17 +66,5 @@ function findTheCheese(recipe) {
  
 }
 
-function wordsWithB(words) {
-    
-    var bWords = [];
-    
-    for (var i = 0; i < words.length; i++) {
-        
-        bWords.push(words.search("b"));
-    }
-    
-    return bWords;
-    
-}
 
 
